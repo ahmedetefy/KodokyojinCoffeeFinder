52ae0dc98f3e480f15a4a373db2a7171a040f1c2
Moustafa Mahmoud <mostafa.93.mahmoud@gmail.com>
2015-03-20 22:43:18 +0200

    fixed some minor issues

--- CoffeeFinder/CoffeeFinderApp/urls.py
+++ CoffeeFinder/CoffeeFinderApp/urls.py
@@ -4,10 +4,7 @@ from CoffeeFinderApp import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^map/', views.map, name='map'),
-
         url(r'^shopSubscribe/', views.shopSubscribe, name = 'shopSubscribe'),
-
-
         url(r'^create_page/', views.create_page, name='create_page'),
         url(r'^page/(?P<page_name_slug>[\w\-]+)/$', views.page, name='page'),
         url(r'^coffee_item_page/(?P<coffee_item_name_id>[\w\-]+)/$', views.coffee_item_page, name='coffee_item_page'),
