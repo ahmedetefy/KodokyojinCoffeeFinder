839750e02b4bf16d3b49a781f0a1ab7df4bb903b
ranael-garem <ranaelgarem781994@gmail.com>
2015-03-20 15:52:57 +0200

    Skeleton

--- CoffeeFinder/CoffeeFinderApp/views.py
+++ CoffeeFinder/CoffeeFinderApp/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-	context_dict = {}
-	return render(request, 'CoffeeFinderApp/index.html', context_dict)
-
-def map(request):
-	context_dict = {}
-	return render(request, 'CoffeeFinderApp/map.html', context_dict)
