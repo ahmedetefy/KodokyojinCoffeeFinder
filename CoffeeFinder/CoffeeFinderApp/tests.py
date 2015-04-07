78f7338c6b33c125aa542f271604f938ad02b231
Ahmed Bastawisi <ahmed.bastawisy@student.guc.edu.eg>
2015-04-07 12:08:56 +0200

    test added to delivery option #25

--- CoffeeFinder/CoffeeFinderApp/tests.py
+++ CoffeeFinder/CoffeeFinderApp/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from CoffeeFinderApp.models import Coffee_item_review
+from CoffeeFinderApp.models import Coffee_item_review, Page
 from django.core.urlresolvers import reverse
 # Create your tests here.
 
@@ -12,11 +12,14 @@ class ReviewMethodTests(TestCase):
         """
                 ensure_reviews_are_positive should results True for reviews where reviews are zero or positive
         """
-        rev = Coffee_item_review(field='test',coffee_item_id=1, user_id=0)
+        rev = Coffee_item_review(field='test', coffee_item_id=1, user_id=0)
         rev.save()
         self.assertEqual((rev.field != 0), True)
 
-
-
-
-        
+    def test_ensure_delivery_option_is_positive_old(self):
+		
+		"""
+		test_ensure_delevery_option_are_positive should results True when page have delivery option
+		"""        
+		page = Page(owner='test owner', name='test coffeeshop', delivery=True)
+		self.assertEqual(page.delivery, True)
