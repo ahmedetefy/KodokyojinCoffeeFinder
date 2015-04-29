1087b8a35b2dbe58ad2f536a9b6abf688aab835a
Aly Yakan <alyyakan@hotmail.com>
2015-04-29 12:29:01 +0300

    Clean up

--- CoffeeFinder/static/js/CharCount.js
+++
@@ -1,8 +0,0 @@
-function countChar(val) {
-    var len = val.value.length;
-    if (len >= 400) {
-        val.value = val.value.substring(0, 400);
-    } else {
-          $('#charNum').text(400 - len);
-        }
-};
